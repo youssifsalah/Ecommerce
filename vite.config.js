3511add166685f5e472074e03d8759fe547b0c75
youssifsalah <youssifsalah9gmail.com>
2025-12-07 01:58:46 +0200

    Fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
-  base: '/Ecommerce/',
+  base: "/",   // ✅ MUST BE ROOT FOR VERCEL
 })
